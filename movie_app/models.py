bd9bc19065fdd1ed7e98901a7ccc6c999f16777d
bnlvdn <bnlvdn7483@gmail.com>
2022-04-14 17:37:34 +0300

    Работа с ОРМ

--- movie_app/models.py
+++ movie_app/models.py
@@ -7,7 +7,30 @@
 Помимо записей в БД через python shell_plus / python shell очевидно можно записывать через сам SQL запрос.
 
 Получать записи из таблицы (SELECT) можно делать из shell_plus:
----НАЗВАНИЕ_КЛАССА.objects.all()--- на выходе получаем сет из объектов. Можно делать запрос по индексу или по срезу"""
+---НАЗВАНИЕ_КЛАССА.objects.all()--- на выходе получаем сет из объектов. Можно делать запрос по индексу или по срезу
+
+Изменять БД можно через БД запросы, а можно через shell, для изменения через shell необходимо создать объект класса в
+котором будет содержаться колонка с контентом из БД:
+---ИМЯ_ОБЪЕКТА = НАЗВАНИЕ_КЛАССА.objects.all()[ИНДЕКС]---
+---ИМЯ_ОБЪЕКТА.ИМЯ_СТОЛБЦА = НОВОЕ_ЗНАЧЕНИЕ---
+---ИМЯ_ОБЪЕКТА.save()      для удаления: ---ИМЯ_ОБЪЕКТА.delete()---
+
+!!!!!!!!!!  ДОКУМЕНТАЦИЯ ПО РАБОТЕ С QUERYSET https://docs.djangoproject.com/en/3.2/ref/models/querysets/
+Фильтрация. Метод get() позволяет получить любую ОДНУ запись с уникальным значением столбца (проще всего исп. с id)
+            Метод filter() может возвращать несколько записей рабоатет как WHERE в SQL (objects.filter(year__isnull=False)) (objects.filter(year__isnull=True, name='Avatar'))
+__contains - СОДЕРЖИТ, __startswith - НАЧИНАЕТСЯ С, __endswith - ЗАКАНЧИВАЕТСЯ, __in - аналог python IN, __gt - БОЛЬШЕ ЧЕМ, __lt - МЕНЬШЕ ЧЕМ
+            Метод create() позволяет создавать объект без метода save(). ---Movie.objects.create(name='Avatar-2', rating=83)---
+            Метод exclude() работает как filter, но он ИСКЛЮЧАЕТ из выборки (как !=)
+
+Метод Q. from django.db import Q. Для работы с объектом Q, Q ставится перед условием и условие оборачивается в скобки.
+---Movie.objects.filter(Q(year__isnull=True)), Q(rating=80)---
+Q может содержать условия AND и OR. AND = &, OR = |, также можно использовать NOT, в этом случае NOT = '~'
+---Movie.objects.filter(Q(year__isnull=True) |  Q(rating=80))---
+также можно использовать NOT, в этом случае NOT = '~'
+---Movie.objects.filter(~Q(year__isnull=True))--- выборка из year НЕ null
+
+
+            """
 
 
 from django.db import models
@@ -28,3 +51,6 @@ class Movie(models.Model):
 
     def __str__(self):                                  # методом __str__ можно поменять тип отображения объектов.
         return f'{self.name} - {self.rating}%'          # метод стр переопределяет поведение экземпляров
+
+
+#  python manage.py shell_plus --print-sql
