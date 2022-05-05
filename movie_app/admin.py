31eb307a6b0b5e33e7b54c3f6a294b6e5b6e8a8c
Zhukovec Artur <bnlvdn7483@gmail.com>
2022-05-06 02:29:05 +0300

    Create two new models, added them to admin panel.

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,11 +1,14 @@
 from django.db.models import QuerySet
 from django.contrib import admin
-from .models import Movie
+from .models import Movie, Director, Actor
+
 """Регистрируем модель в админке. в Аргументах необходимо передать название модели
 Для отображения колонок таблицы в адми панели необходимо создать класс в admin.py в котором с добавлением "Admin"
 в название класса, который наследуется от admin.ModelAdmin 
 --- class MovieAdmin(admin.ModelAdmin): ---
 - list_display = ['', '', ''] - список с названиями полей которые мы хотим видеть"""
+
+
 # Register your models here.
 
 
@@ -26,7 +29,7 @@ class RatingFilter(admin.SimpleListFilter):
         """В self.value() Подставляется первое значение из кортежа для правильного использования фильтров
         вспоминаем Django ORM filter"""
         if self.value() == '<50':
-            return queryset.filter(rating__lt=50)   # Команды из Django ORM | filter
+            return queryset.filter(rating__lt=50)  # Команды из Django ORM | filter
         if self.value() == 'от 50 до 65':
             return queryset.filter(rating__gte=50).filter(rating__lt=65)
         if self.value() == 'от 66 до 79':
@@ -39,18 +42,20 @@ class RatingFilter(admin.SimpleListFilter):
 class MovieAdmin(admin.ModelAdmin):
     """Все аргументы данного класса в той или иной форме влияют на отображение
     полей в админке."""
-    list_display = ['name', 'rating', 'currency', 'budget', 'rating_description'] # отображаются в таблице Movies
-    list_editable = ['rating', 'currency', 'budget']
+    list_display = ['name', 'rating', 'budget', 'rating_description', 'director']  # отображаются в таблице Movies
+    list_editable = ['rating', 'budget', 'director']                                # Позволяет изменять данные
     # fields = ['name', 'rating']  # Поля, по которым будет добавляться новая запись в админке.
-    exclude = ['slug'] # Исключает поле slug при создании
+    exclude = ['slug']  # Исключает поле slug при создании
     # readonly_fields = ['year'] # запрещает редактировать это поле при создании
     ordering = ['-rating', 'name']  # Сортировка
-    list_per_page = 10      # Количество элементов, отображаемых на странице.
+    list_per_page = 10  # Количество элементов, отображаемых на странице.
     actions = ['set_dollars', 'set_euro', 'set_rubles']
-    search_fields = ['name', ]  # Поля по которым будет осуществляться поиск
-    list_filter = [RatingFilter]  # фильтр по следующим полям.
+    search_fields = ['name', ]        # Поля по которым будет осуществляться поиск
+    list_filter = [RatingFilter]      # фильтр по следующим полям.
+    filter_horizontal = ['actors']    # Для отображения поля Актёров в редактировании при связи many to many
 
-    @admin.display(ordering='rating', description='рекомендации') # ДЛЯ ВОЗМОЖНОСТИ СОРТИРОВКИ В АДМИН ПАНЕЛИ. descr= ДЛЯ ОТОБРАЖЕНИЯ НАЗВ. КОЛОНКИ
+    @admin.display(ordering='rating',
+                   description='рекомендации')  # ДЛЯ ВОЗМОЖНОСТИ СОРТИРОВКИ В АДМИН ПАНЕЛИ. descr= ДЛЯ ОТОБРАЖЕНИЯ НАЗВ. КОЛОНКИ
     def rating_description(self, movie: Movie):
         if movie.rating < 68:
             return "Не рекомендовано к просмотру."
@@ -84,6 +89,5 @@ class MovieAdmin(admin.ModelAdmin):
         )
 
 
+admin.site.register((Actor, Director))      # Можно регать, передавая аргументы через кортеж
 
-
-#  admin.site.register(Movie, MovieAdmin)  # Заменено декоратором
